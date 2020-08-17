f2f78e807503b05e2b293f726dc02f604bb4b319
coderquill <coderquill@gmail.com>
2020-08-17 21:22:39 +0530

    initialize project

---
+++ demoProduct/src/test/java/com/rupali/demoProduct/DemoProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rupali.demoProduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
