558ea6871081e5450df1b747a69eb75004b7fd9e
coderquill <coderquill@gmail.com>
2020-08-17 22:31:29 +0530

    create employee repository

---
+++ employeeCRUD/src/main/java/com/rupali/employeeCRUD/EmployeetRepository.java
@@ -0,0 +1,7 @@
+package com.rupali.employeeCRUD;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeetRepository extends JpaRepository<Employee, Integer> {
+
+}
