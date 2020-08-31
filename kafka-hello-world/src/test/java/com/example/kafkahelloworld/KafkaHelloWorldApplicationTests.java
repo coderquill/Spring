c8e260efd081a8cc0325ac240b0466dd31672abe
coderquill <coderquill@gmail.com>
2020-09-01 00:06:44 +0530

    spring-mysql-redis-rabbitmq crud

---
+++ kafka-hello-world/src/test/java/com/example/kafkahelloworld/KafkaHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkahelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
